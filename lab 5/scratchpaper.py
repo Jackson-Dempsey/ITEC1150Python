95183672ce7b0100283fb8a7ccd164d960bba090
Jackson Dempsey <ue8706sr@go.minnstate.edu>
2023-10-04 11:59:37 -0500

    Update scratchpaper.py
    honestly pretty confused, tried to add another varible to account for how much a user would want to count by, didn't end up working too great, ik i can prolly make this better, just gonna leave this for now since i don't have more time atm...

--- lab 5/scratchpaper.py
+++ lab 5/scratchpaper.py
@@ -6,14 +6,15 @@ def main():
 
 
 
-    #these two lines refrence the variables which the inputs and processing functions "returned" to us below
-    beginingNumber, endingNumber = inputs()
-    userCountingTrack = processing
+    #this refrences the variables which the inputs and processing functions "returned" to us below
+    beginingNumber, endingNumber, countAmt = inputs()
 
     #because we refrenced them, we can now input them like the variables we've done before
     
-    processing(beginingNumber,endingNumber) 
-    outputs(userCountingTrack) #outputs here is essentially a fancy print statment, since that function only has a print statement inside it
+    processing(beginingNumber,endingNumber, countAmt)
+    #debugging rn, processing will only print in the processing function
+
+    #outputs(userCountingTrack) #outputs here is essentially a fancy print statment, since that function only has a print statement inside it
 
     
 ## IPO part, where main gets it's stuff to do
@@ -24,20 +25,21 @@ def inputs():
     beginingNumber = int(input("enter a whole number greater than 0: "))
     print("what number do you want to end with?")
     endingNumber = int(input("enter a whole number greater than 0: "))
+    print("how much do you want to count by? (you can count by negitive numbers too!)")
+    countAmt = int(input("enter a whole number greater than 0: "))
 
-    return beginingNumber, endingNumber
+
+    return beginingNumber, endingNumber, countAmt
 
 
 
-def processing(startNumber,endNumber):
+def processing(startNumber,endNumber,countAmt):
     
-
+    #defining and setting a user counting track variable equal to zero so python can add the sum of the numbers later
     userCountingTrack = 0
 
-    if startNumber < endNumber:
-        stepDirectonIndicator = 1
 
-    for count in range(startNumber,endNumber,stepDirectonIndicator): #this code then will repeatedly iterate the following untill it reaches the specified end number:
+    for count in range(startNumber,endNumber-countAmt,countAmt): #this code then will repeatedly iterate the following untill it reaches the specified end number:
 
         startNumber += 1 #it will add one to the the value stored in userBeginingNumber
 
